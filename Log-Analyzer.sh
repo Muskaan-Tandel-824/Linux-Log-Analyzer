b874200ae5cc2f21ab5377d811f98d68dd787042
root <root@ip-172-31-19-73.ap-south-1.compute.internal>
2026-02-17 18:17:21 +0000

    Added linux log analysis project

---
+++ Log-Analyzer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "========== Linux Log Analyzer =========="
+echo "Hostname: $(hostname)"
+echo "Date: $(date)"
+echo "----------------------------------------"
+
+echo "Failed SSH Login Attempts:"
+sudo journalctl | grep "Failed password" | wc -l
+echo "----------------------------------------"
+
+echo "Top 5 Failed Login IPs:"
+sudo journalctl | grep "Failed password" | awk '{print $(NF-3)}' | sort | uniq -c | sort -nr | head -5
+echo "----------------------------------------"
+
+echo "Total Error Messages:"
+sudo journalctl | grep -i error | wc -l
+echo "----------------------------------------"
+
